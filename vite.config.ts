7ed7fd8a90f144a0b01fbbb59536dcc71c6f4350
maurolsmoura <maurolsmoura@gmail.com>
2025-03-05 16:59:11 -0300

    adjusted slashes

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // base: mode === "development" ? "/" : "/vendas-mauro-carol/", // Allows local development without issues
+  base: "/vendas-mauro-carol",
   build: {
     outDir: "dist", // Ensures build output is in the correct folder
   },
